94d4cb4bb71d04fb08c3ab1986a5fe78d48018cf
Natali <nanatkim11@gmail.com>
2021-03-19 11:34:03 -0300

    [feat/front] - add search input to home and handle navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
-// import { Container } from './styles';
+import { Container, Wrapper, SearchForm } from "./styles";
 
 const Home: React.FC = () => {
-  return <div />;
-}
+  const [search, setSearch] = useState("");
+  const history = useHistory();
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    history.push("/" + search.toLocaleLowerCase().trim());
+  }
+
+  return (
+    <Container>
+      <Wrapper>
+        <span>github.com/</span>
+        <SearchForm onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="octocat"
+            value={search}
+            onChange={(e) => setSearch(e.currentTarget.value)}
+          />
+          <span onClick={handleSubmit}>▷</span>
+        </SearchForm>
+      </Wrapper>
+    </Container>
+  );
+};
 
 export default Home;
--- frontend/src/pages/Home/styles.ts
+++ frontend/src/pages/Home/styles.ts
@@ -1,5 +1,44 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-  
+  height: 100vh;
+  position: relative;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+
+  span {
+    font-size: 1.5rem;
+  }
+`;
+
+export const SearchForm = styled.form`
+  display: flex;
+  align-items: center;
+  border: 1px solid #ccc;
+  margin-left: 1rem;
+
+  input {
+    padding: 1rem;
+    width: 150px;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  span {
+    font-size: 1.5rem;
+    margin: .5rem;
+    color: var(--blue);
+    cursor: pointer;
+  }
 `;
